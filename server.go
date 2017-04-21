0d1a66a8ff30b7becde5e9c12116ef47125faf07
MarcManiez <marc.maniez@gmail.com>
2017-04-21 10:12:39 -0700

    Integrate Twitter streaming API.

--- server.go
+++ server.go
@@ -1,13 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 	"github.com/gorilla/websocket"
 )
 
 type we struct{}
+
+var config = oauth1.NewConfig(os.Getenv("TWITTER_API_KEY_BRANDLESS"), os.Getenv("TWITTER_API_SECRET_BRANDLESS"))
+var token = oauth1.NewToken(os.Getenv("TWITTER_TOKEN"), os.Getenv("TWITTER_SECRET"))
+var httpClient = config.Client(oauth1.NoContext, token)
+var client = twitter.NewClient(httpClient)
+var params = &twitter.StreamFilterParams{
+	Track:         []string{"kitten"},
+	StallWarnings: twitter.Bool(true),
+}
+var stream, err = client.Streams.Filter(params)
 
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
@@ -24,10 +40,10 @@ func (yo we) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		messageType, p, err := conn.ReadMessage()
 		if err != nil {
 			log.Println(err)
-		} else {
-			log.Println(messageType)
-			log.Println(string(p))
+			return
 		}
+		log.Println(messageType)
+		log.Println(string(p))
 		p = []byte("hey can you read this?")
 		if err = conn.WriteMessage(messageType, p); err != nil {
 			log.Println(err)
@@ -36,6 +52,24 @@ func (yo we) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+
+	demux := twitter.NewSwitchDemux()
+	demux.Tweet = func(tweet *twitter.Tweet) {
+		fmt.Println(tweet.Text)
+	}
+
+	go demux.HandleChan(stream.Messages)
+
+	// Wait for SIGINT and SIGTERM (HIT CTRL-C)
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	log.Println(<-ch)
+
+	log.Println("Stopping Stream...")
+	stream.Stop()
+
+	// ==================
+
 	fs := http.FileServer(http.Dir("client"))
 	http.Handle("/", fs)
 	var lol we
