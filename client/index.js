5bb28befa12f1889771cefff8e47a7480427f385
MarcManiez <marc.maniez@gmail.com>
2017-04-21 13:25:22 -0700

    Change socket connection's name.

--- client/index.js
+++ client/index.js
@@ -1,10 +1,9 @@
-const socket = new WebSocket('ws://localhost:3000/hey');
+const socket = new WebSocket('ws://localhost:3000/tweets');
 
 socket.addEventListener('open', function (event) {
   socket.send('Hello Server!');
 });
 
-// Listen for messages
 socket.addEventListener('message', function (event) {
   console.log('Message from server', event.data);
 });
