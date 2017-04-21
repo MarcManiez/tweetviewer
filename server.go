91bdc7436adbc79fe0d093e6b92a79e97e24440e
MarcManiez <marc.maniez@gmail.com>
2017-04-21 12:44:34 -0700

    Fit Twitter stream creation into its own function.

--- server.go
+++ server.go
@@ -13,22 +13,13 @@ import (
 
 type handler struct{}
 
-var config = oauth1.NewConfig(os.Getenv("TWITTER_API_KEY_BRANDLESS"), os.Getenv("TWITTER_API_SECRET_BRANDLESS"))
-var token = oauth1.NewToken(os.Getenv("TWITTER_TOKEN"), os.Getenv("TWITTER_SECRET"))
-var httpClient = config.Client(oauth1.NoContext, token)
-var client = twitter.NewClient(httpClient)
-var params = &twitter.StreamFilterParams{
-	Track:         []string{"kitten"},
-	StallWarnings: twitter.Bool(true),
-}
-var stream, err = client.Streams.Filter(params)
-
-var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
-}
-
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+
+	upgrader := websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
@@ -49,16 +40,31 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func getStream(params *twitter.StreamFilterParams) (*twitter.Stream, error) {
+	config := oauth1.NewConfig(os.Getenv("TWITTER_API_KEY_BRANDLESS"), os.Getenv("TWITTER_API_SECRET_BRANDLESS"))
+	token := oauth1.NewToken(os.Getenv("TWITTER_TOKEN"), os.Getenv("TWITTER_SECRET"))
+	httpClient := config.Client(oauth1.NoContext, token)
+	client := twitter.NewClient(httpClient)
+	return client.Streams.Filter(params)
+}
+
 func readTweets(s *twitter.Stream) {
 	demux := twitter.NewSwitchDemux()
 	demux.Tweet = func(tweet *twitter.Tweet) {
 		fmt.Println(tweet.Text)
 	}
-	go demux.HandleChan(stream.Messages)
+	go demux.HandleChan(s.Messages)
 }
 
 func main() {
-
+	params := &twitter.StreamFilterParams{
+		Track:         []string{"kitten"},
+		StallWarnings: twitter.Bool(true),
+	}
+	stream, err := getStream(params)
+	if err != nil {
+		panic(err)
+	}
 	go readTweets(stream)
 
 	// ==================
