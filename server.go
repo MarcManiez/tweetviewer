f116904f71a501e2dd1cabe0deabbd69685bbf3c
MarcManiez <marc.maniez@gmail.com>
2017-04-21 17:39:36 -0700

    Build new parameters.

--- server.go
+++ server.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"bytes"
-	"encoding/gob"
 	"fmt"
 	"log"
 	"net/http"
@@ -12,16 +10,6 @@ import (
 	"github.com/dghubble/oauth1"
 	"github.com/gorilla/websocket"
 )
-
-func GetBytes(key interface{}) ([]byte, error) {
-	var buf bytes.Buffer
-	enc := gob.NewEncoder(&buf)
-	err := enc.Encode(key)
-	if err != nil {
-		return nil, err
-	}
-	return buf.Bytes(), nil
-}
 
 type handler struct {
 	stream *twitter.Stream
@@ -70,7 +58,8 @@ func readTweets(s *twitter.Stream) {
 
 func main() {
 	params := &twitter.StreamFilterParams{
-		Track:         []string{"kitten"},
+		Track:         []string{"brands && advertising", "consumerism"},
+		Follow:        []string{"12480582", "22151553", "267399199", "308452020", "17540485", "26787673", "713033114092761088", "Colgate", "21778607", "485071945", "138845026", "23085995", "570715775", "109224937", "17137891", "17475575", "280557152", "2874230781", "17899654", "534249758", "92793164", "2347131241", "12405142", "19720440", "19720019", "57013560", "398942686", "831488280", "213299248", "126084292", "51119925", "31143489", "347958019", "71026122", "151913390", "20758087", "20094535", "92294003"},
 		StallWarnings: twitter.Bool(true),
 	}
 	stream, err := getStream(params)
