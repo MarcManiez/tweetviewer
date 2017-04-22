8539f054465b0cff9a3f7f4315b8cece346176f5
MarcManiez <marc.maniez@gmail.com>
2017-04-21 19:17:57 -0700

    Invoke React app.

--- client/index.js
+++ client/index.js
@@ -1,19 +1,28 @@
-const socket = new WebSocket('ws://localhost:3000/tweets');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 
-socket.addEventListener('open', function (event) {
-  socket.send('Hello Server!');
-});
+const div = document.createElement('div');
+div.setAttribute('id', 'app');
+document.body.append(div);
+ReactDOM.render(<App />, document.getElementById('app'));
 
-socket.addEventListener('message', function (event) {
-  console.log('Message from server', event.data);
-});
+// const socket = new WebSocket('ws://localhost:3000/tweets');
 
-let counter = 0;
-const spanerino = document.querySelector('span');
-spanerino.innerHTML = counter;
-const button = document.querySelector('button');
-const clickHandler = (e) => {
-  spanerino.innerHTML = ++counter;
-  socket.send('OMG I\'M DOING STUFF');
-};
-button.onclick = clickHandler;
+// socket.addEventListener('open', function (event) {
+//   socket.send('Hello Server!');
+// });
+
+// socket.addEventListener('message', function (event) {
+//   console.log('Message from server', event.data);
+// });
+
+// let counter = 0;
+// const spanerino = document.querySelector('span');
+// spanerino.innerHTML = counter;
+// const button = document.querySelector('button');
+// const clickHandler = (e) => {
+//   spanerino.innerHTML = ++counter;
+//   socket.send('OMG I\'M DOING STUFF');
+// };
+// button.onclick = clickHandler;
