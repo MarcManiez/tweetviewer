a4ae295807f15a474cdf65c152638bc90a1b8e8a
MarcManiez <marc.maniez@gmail.com>
2017-04-21 21:20:09 -0700

    Make Timeline open a websocket.

--- client/components/Timeline.jsx
+++ client/components/Timeline.jsx
@@ -7,10 +7,31 @@ export default class Timeline extends React.Component {
     super(props);
     this.state = { tweets: [] };
   }
+
+  componentWillMount() {
+    const socket = new WebSocket('ws://localhost:3000/tweets');
+    console.log('consumerism...');
+
+    socket.addEventListener('open', function (event) {
+      socket.send('Hello Server!');
+    });
+
+    socket.addEventListener('message', (event) => {
+      console.log(this.state.tweets);
+      if (this.state.tweets.length > 10) {
+        this.state.tweets.pop();
+      }
+      this.state.tweets.unshift(event.data);
+      console.log('Message from server', event.data);
+    });
+  }
+
   render() {
     return (
       <div>
-        This is the timeline.
+        {
+          this.state.tweets.map((tweet, index) => <Tweet tweet={tweet} key={index} />)
+        }
       </div>
     )
   }
