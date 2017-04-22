53c9ec5cca3c093c5c273c6ecc97a32c1a6c17a0
MarcManiez <marc.maniez@gmail.com>
2017-04-22 11:02:51 -0700

    Add structure to Tweets.

--- client/components/Tweet.jsx
+++ client/components/Tweet.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 
 const Tweet = ({ tweet }) => (
-  <div>
-    {tweet.text}
+  <div className="tweet">
+    <img src={tweet.user.profile_image_url} alt="twitter avatar" className="avatar"/>
+    <div className="meta">
+      <strong>@<a href={`https://twitter.com/${tweet.user.screen_name}`}>{tweet.user.screen_name}</a></strong><small className="time"><time className="timeago" dateTime={tweet.created_at}>{tweet.created_at.split(' ').slice(0, 4).join(' ')}</time></small>
+    </div>
+    <p className="text">{tweet.text}</p>
   </div>
 );
 
