14c8e22bef779f136e48f79904c1faa7d726fb82
MarcManiez <marc.maniez@gmail.com>
2017-04-21 22:40:53 -0700

    Clear zombie code.

--- client/index.js
+++ client/index.js
@@ -6,23 +6,3 @@ const div = document.createElement('div');
 div.setAttribute('id', 'app');
 document.body.append(div);
 ReactDOM.render(<App />, document.getElementById('app'));
-
-// const socket = new WebSocket('ws://localhost:3000/tweets');
-
-// socket.addEventListener('open', function (event) {
-//   socket.send('Hello Server!');
-// });
-
-// socket.addEventListener('message', function (event) {
-//   console.log('Message from server', event.data);
-// });
-
-// let counter = 0;
-// const spanerino = document.querySelector('span');
-// spanerino.innerHTML = counter;
-// const button = document.querySelector('button');
-// const clickHandler = (e) => {
-//   spanerino.innerHTML = ++counter;
-//   socket.send('OMG I\'M DOING STUFF');
-// };
-// button.onclick = clickHandler;
