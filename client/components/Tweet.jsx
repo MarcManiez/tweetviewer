02cca9803d91157b98325693c78b5a6899d84551
MarcManiez <marc.maniez@gmail.com>
2017-04-22 16:26:31 -0700

    Display https images.

--- client/components/Tweet.jsx
+++ client/components/Tweet.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Tweet = ({ tweet }) => (
   <div className="tweet">
-    <img src={tweet.user.profile_image_url} alt="twitter avatar" className="avatar"/>
+    <img src={tweet.user.profile_image_url_https} alt="twitter avatar" className="avatar"/>
     <div className="meta">
-      <strong>@<a href={`https://twitter.com/${tweet.user.screen_name}`}>{tweet.user.screen_name}</a></strong><small className="time"><time className="timeago" dateTime={tweet.created_at}>{tweet.created_at.split(' ').slice(0, 4).join(' ')}</time></small>
+      <strong>@<a href={`https://twitter.com/${tweet.user.screen_name}`}>{tweet.user.screen_name}</a></strong>
+      <small className="time"><time className="timeago" dateTime={tweet.created_at}>{tweet.created_at.split(' ').slice(0, 4).join(' ')}</time></small>
     </div>
     <p className="text">{tweet.text}</p>
   </div>
